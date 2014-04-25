c709aad839b0be4aaef91e778a87e536f7b0e591
James Duffy <jamesmartinduffy@gmail.com>
2014-04-25 17:28:45 +0900

    Nevermind, putting all changes back into one file

--- db/migrate/20140425060207_add_korean_address_fields_to_spree_address.rb
+++ db/migrate/20140425060207_add_korean_address_fields_to_spree_address.rb
@@ -2,5 +2,6 @@ class AddKoreanAddressFieldsToSpreeAddress < ActiveRecord::Migration
   def change
     add_column :spree_addresses, :customs_no, :string
     add_column :spree_addresses, :jumin_no, :string
+    add_column :spree_addresses, :other_comment, :string
   end
 end
--- db/migrate/20140425081803_add_other_comment_to_spree_address.rb
+++
@@ -1,5 +0,0 @@
-class AddOtherCommentToSpreeAddress < ActiveRecord::Migration
-  def change
-    add_column :spree_addresses, :other_comment, :string
-  end
-end
