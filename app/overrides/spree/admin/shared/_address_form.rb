efcdde40a23b579eb139d945ac24419fd14f5964
James Duffy <jamesmartinduffy@gmail.com>
2014-07-08 11:45:58 +0900

    Added "other_comment" field to admin address form

--- app/overrides/spree/admin/shared/_address_form.rb
+++ app/overrides/spree/admin/shared/_address_form.rb
@@ -7,4 +7,8 @@ Deface::Override.new(
       <%= f.label :customs_no, Spree.t(:customs_no) %>
       <%= f.text_field :customs_no, :class => 'fullwidth' %>
     </div>
+    <div class='field billing-row'>
+      <%= f.label :other_comment, Spree.t(:other_comment) %>
+      <%= f.text_field :other_comment, :class => 'fullwidth' %>
+    </div>
   ")
